d5632e8520649ac13f666cf784e41bf0ecb774f0
Web Produtora <webprodutora@vps26532.publiccloud.com.br>
2021-04-18 02:32:17 -0300

    v2.0.3

--- src/bin/generators/Producer.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-namespace Albreis\DummyApp\Producers;
-
-use Albreis\Kurin\Producer;
-
-/** @package Albreis\DummyApp\Models */
-class DummyClasssProducer extends Producer {
-  public $model = "Albreis\DummyApp\Models\DummyClass";
-}
